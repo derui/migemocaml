a24d6330c3a0b6b8688babe145ce513ad5ddc0c2
derui <derutakayu@gmail.com>
2018-06-09 09:35:29 +0900

    fix: use valid size to get tail of string

--- src/util.ml
+++ src/util.ml
@@ -3,5 +3,5 @@
 (** Take first string of [size] from [str] *)
 let take ?(size=1) str =
   if String.length str = 0 then ("", "")
-  else if String.length str < size then (str, "")
-  else (String.sub str 0 size, String.(sub str size @@ length str - 1))
+  else if String.length str <= size then (str, "")
+  else (String.sub str 0 size, String.(sub str size @@ length str - size))
