384c80df737ff3220b4edf11845b243c23f49bc4
derui <derutakayu@gmail.com>
2021-09-04 09:36:47 +0900

    fix: update options in benchmark script

--- misc/benchmark.sh
+++ misc/benchmark.sh
@@ -8,30 +8,30 @@ words=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 2 | head -n $count | sor
 
 echo "Word size: 2"
 echo "C/Migemo:"
-time echo "$words" | cmigemo -d /usr/share/migemo/utf-8/migemo-dict > /dev/null
+time echo "$words" | cmigemo -d /usr/share/migemo/migemo-dict > /dev/null
 echo "Migemocaml:"
-time echo "$words" | _build/default/bin/main.exe -d /usr/share/migemo/utf-8 > /dev/null
+time echo "$words" | _build/default/bin/main.exe -d /usr/share/migemo > /dev/null
 
 words=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n $count | sort | uniq)
 
 echo "Word size: 4"
 echo "C/Migemo:"
-time echo "$words" | cmigemo -d /usr/share/migemo/utf-8/migemo-dict > /dev/null
+time echo "$words" | cmigemo -d /usr/share/migemo/migemo-dict > /dev/null
 echo "Migemocaml:"
-time echo "$words" | _build/default/bin/main.exe -d /usr/share/migemo/utf-8 > /dev/null
+time echo "$words" | _build/default/bin/main.exe -d /usr/share/migemo > /dev/null
 
 words=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n $count | sort | uniq)
 
 echo "Word size: 8"
 echo "C/Migemo:"
-time echo "$words" | cmigemo -d /usr/share/migemo/utf-8/migemo-dict > /dev/null
+time echo "$words" | cmigemo -d /usr/share/migemo/migemo-dict > /dev/null
 echo "Migemocaml:"
-time echo "$words" | _build/default/bin/main.exe -d /usr/share/migemo/utf-8 > /dev/null
+time echo "$words" | _build/default/bin/main.exe -d /usr/share/migemo > /dev/null
 
 words=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n $count | sort | uniq)
 
 echo "Word size: 16"
 echo "C/Migemo:"
-time echo "$words" | cmigemo -d /usr/share/migemo/utf-8/migemo-dict > /dev/null
+time echo "$words" | cmigemo -d /usr/share/migemo/migemo-dict > /dev/null
 echo "Migemocaml:"
-time echo "$words" | _build/default/bin/main.exe -d /usr/share/migemo/utf-8 > /dev/null
+time echo "$words" | _build/default/bin/main.exe -d /usr/share/migemo > /dev/null
